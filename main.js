e3f3f547020ebc58d98df55b09b9ea7ecedeae51
Luigi Minardi <lugminsinlag@gmail.com>
2022-12-02 02:58:03 +0100

    feat: Fazendo com que o botão de próximo retorne 1000 resultados.

--- main.js
+++ main.js
@@ -22,6 +22,35 @@ const previousPageButton = document.getElementById("previousbtn");
 const nextPageButton = document.getElementById("nextbtn");
 
 let PAGE_NUMBER = 1;
+let RANGE = 10;
+
+function fetchNewPostsByRange(newPosts, range = 1){
+  if (!newPosts) {
+    throw new Error('Você precisa de passar um array como primeiro argumento dessa função pra funcionar. Lembre de salvar o array em uma variavel.')
+  }
+
+  const timeOutPromise = new Promise(function(resolve) {
+    setTimeout(resolve, 500, 'Timeout Done');
+  });
+
+  const posts = fetchTabs(contentType.value, PAGE_NUMBER).then(data => {
+    newPosts.push(...data)
+  });
+
+  return Promise.all(
+  [posts, timeOutPromise]).then(()=> {
+    if (PAGE_NUMBER < range-1) {
+      PAGE_NUMBER++
+      console.log('promisses', newPosts.flat())
+      return fetchNewPostsByRange(newPosts, range);
+    } else {
+      console.log('elsed')
+      return newPosts.flat()
+    }
+  })
+
+}
+
 
 contentType.addEventListener("change", (e) => {
   resetContent(async () => {
@@ -29,7 +58,10 @@ contentType.addEventListener("change", (e) => {
 
     PAGE_NUMBER = 1; // Reset PAGE_NUMBER when change the category.
 
-    const newPosts = await fetchTabs(e.target.value, PAGE_NUMBER);
+    let postsArray = []
+    let range = PAGE_NUMBER + RANGE
+
+    const newPosts = await fetchNewPostsByRange(postsArray, range).then(data => data)
 
     renderTabs(newPosts, () => {
       if (searchBar.value.length != 0) {
@@ -53,8 +85,8 @@ nextPageButton.addEventListener("click", goToNextPage);
 
 function goToPreviousPage() {
   if (PAGE_NUMBER > 1) {
-    previousPage(PAGE_NUMBER, (newPage) => {
-      PAGE_NUMBER = newPage;
+    previousPage(PAGE_NUMBER, () => {
+      PAGE_NUMBER--;
 
       resetContent(async () => {
         disableOperators();
@@ -74,20 +106,23 @@ function goToPreviousPage() {
 }
 
 function goToNextPage() {
-  nextPage(PAGE_NUMBER, (newPage) => {
-    PAGE_NUMBER = newPage;
+  nextPage(PAGE_NUMBER, () => {
+    PAGE_NUMBER++;
 
     resetContent(async () => {
       disableOperators();
 
-      const newPosts = await fetchTabs(contentType.value, PAGE_NUMBER);
+      let postsArray = []
+      let range = PAGE_NUMBER + RANGE
 
-      renderTabs(newPosts, () => {
-        if (searchBar.value.length != 0) {
-          searchTabs(searchBar.value);
-        }
+      const newPosts = await fetchNewPostsByRange(postsArray, range).then(data => data)
 
-        enableOperators();
+        renderTabs(newPosts, () => {
+          if (searchBar.value.length != 0) {
+            searchTabs(searchBar.value);
+          }
+
+          enableOperators();
       });
     });
   });
